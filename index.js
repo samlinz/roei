96f02a92523fc823a27fb3054a2c330beec179bb
Samuel Lindqvist <samuel.lindqvist@R3543.reaktor.tku>
2023-04-19 16:19:14 +0300

    Refactoring, new features

--- index.js
+++ index.js
@@ -1,42 +1,26 @@
 #!/usr/bin/env node
 
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
 const dotenv = require("dotenv");
+const { exec } = require("child_process");
 const { parseISO, formatISO, format } = require("date-fns");
 const { log } = require("console");
 dotenv.config();
 
-const _getConfig = (configFile) => {
-  const f1 = fs.readFileSync(configFile);
+const _getConfig = async (configFile) => {
+  const f1 = await fs.readFile(configFile);
   const f = JSON.parse(f1);
   return (name) => {
     return f[name] || f[name.toLowerCase()];
   };
 };
-const getConfig = _getConfig(`${__dirname}/.config.json`);
 
 const getDirFullPath = (dir) => {
   const isAbsolute = dir.startsWith("/");
   if (isAbsolute) return dir;
   return `${__dirname}/${dir}`;
 };
-
-const FILE = getConfig("file");
-const filePathFull = getDirFullPath(FILE);
-
-// Create file if doesn't exists
-if (!fs.existsSync(filePathFull)) {
-  fs.openSync(filePathFull, "w");
-  console.log(`File created: ${filePathFull}`);
-}
-
-// Command line args
-const args = [...process.argv];
-const params = args.slice(2);
-const [cmd1, cmd2, cmd3, cmd4] = params;
-const isCommandList = cmd1 === "list";
-const isCommandLog = !isCommandList;
 
 // Utility functions
 const isTimeValid = (str) => {
@@ -53,28 +37,20 @@ const normalizeCategory = (str) => {
   return str.trim().toUpperCase();
 };
 
-const getCategory = (str) => {
-  const categories = getConfig("categories") || {};
-  const alias = getConfig("alias") || {};
-  for (const cat of Object.keys(categories)) {
-    // const aliases = alias[cat] || [];
-    const config = categories[cat];
-    const aliases = config.alias || [];
-    // console.log({
-    //   cat,
-    //   categories,
-    //   alias,
-    //   aliases,
-    //   str,
-    //   asdfafs: normalizeCategory(str),
-    // });
-    const categoryNormalized = normalizeCategory(cat);
-    if (normalizeCategory(str) === categoryNormalized)
-      return categoryNormalized;
-    if (aliases.includes(str)) return categoryNormalized;
-  }
-  return null;
-};
+const getCategory =
+  ({ getConfig }) =>
+  (str) => {
+    const categories = getConfig("categories") || {};
+    for (const cat of Object.keys(categories)) {
+      const config = categories[cat];
+      const aliases = config.alias || [];
+      const categoryNormalized = normalizeCategory(cat);
+      if (normalizeCategory(str) === categoryNormalized)
+        return categoryNormalized;
+      if (aliases.includes(str)) return categoryNormalized;
+    }
+    return null;
+  };
 
 const getFormattedDate = (date) => {
   //   return formatISO(date);
@@ -102,59 +78,166 @@ const parseRow = (row) => {
   };
 };
 
-if (isCommandList) {
-  console.log("list");
-} else if (isCommandLog) {
-  const isFirstCommandLog = cmd1 === "log";
-  const _category = isFirstCommandLog ? cmd2 : cmd1;
-  const time = isFirstCommandLog ? cmd3 : cmd2;
-  const timeValid = isTimeValid(time);
-  const desc = isFirstCommandLog
-    ? timeValid
-      ? params.slice(3).join(" ")
-      : params.slice(2).join(" ")
-    : timeValid
-    ? params.slice(2).join(" ")
-    : params.slice(1).join(" ");
+const getRows = async ({ file }) => {
+  const oldRows = await fs.readFile(file, "utf-8");
+  const splitRows = oldRows.length === 0 ? [] : oldRows.split("\n");
+  const newRows = [...splitRows];
+  return newRows;
+};
 
-  // Validate category
-  const category = getCategory(_category);
-  if (!category) {
-    console.error(`Invalid category: ${_category}`);
-    process.exit(1);
+const fileExists = async (file) => {
+  try {
+    await fs.access(file, fs.constants.F_OK);
+    return true;
+  } catch (err) {
+    return false;
   }
+};
 
-  // Create log row
-  const getFullDate = getFullDateForTime(time);
-  const fullRow = `${getFullDate}; ${category.toUpperCase()}; ${desc || "-"}`;
+const doBackup = async ({ file }) => {
+  const backupDir = `${__dirname}/backups`;
+  if (!(await fileExists(backupDir))) {
+    fs.makeDirSync(backupDir);
+  }
+  const fileBaseName = path.basename(file);
+  const backupFile = `${Date.now()}_${fileBaseName}`;
+  await fs.copyFile(file, `${backupDir}/${backupFile}`);
+  // console.log(`Backup created: ${backupFile}`);
+};
 
-  {
-    const backupDir = `${__dirname}/backups`;
-    if (!fs.existsSync(backupDir)) {
-      fs.makeDirSync(backupDir);
+const handleList = async ({ file }) => {};
+
+const writeRows = async ({ rows, file }) => {
+  const data = rows.join("\n");
+  await fs.writeFile(file, data);
+};
+
+const handleRemove = async ({ file }) => {
+  await doBackup({ file });
+  const rows = await getRows({ file });
+  let lastRow = rows.pop();
+  if (lastRow.startsWith("=")) {
+    lastRow = rows.pop();
+  }
+  await writeRows({ rows, file });
+  console.log(`REMOVED ${lastRow}`);
+};
+
+const handleOpen = async ({ file }) => {
+  console.log(`Opening file: ${file}`);
+  //   const cmd = ["open", file];
+  //   const cmd = [
+  //     "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl",
+  //     ["-n", file],
+  //   ];
+  const cmd = [
+    "'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'",
+    file,
+  ].join(" ");
+  log(cmd);
+  await exec(cmd);
+};
+
+const handleAddLog =
+  ({ getConfig }) =>
+  async ({ file, params }) => {
+    const [cmd1, cmd2, cmd3, cmd4] = params;
+
+    const isFirstCommandLog = cmd1 === "log";
+    const _category = isFirstCommandLog ? cmd2 : cmd1;
+    const time = isFirstCommandLog ? cmd3 : cmd2;
+    const timeValid = isTimeValid(time);
+    const desc = isFirstCommandLog
+      ? timeValid
+        ? params.slice(3).join(" ")
+        : params.slice(2).join(" ")
+      : timeValid
+      ? params.slice(2).join(" ")
+      : params.slice(1).join(" ");
+
+    // Validate category
+    const category = getCategory({ getConfig })(_category);
+    if (!category) {
+      console.error(`Invalid category: ${_category}`);
+      process.exit(1);
     }
-    const fileBaseName = path.basename(FILE);
-    const backupFile = `${Date.now()}_${fileBaseName}`;
-    fs.copyFileSync(filePathFull, `${backupDir}/${backupFile}`);
-    // console.log(`Backup created: ${backupFile}`);
-  }
 
-  {
-    const oldRows = fs.readFileSync(filePathFull, "utf-8");
-    const splitRows = oldRows.length === 0 ? [] : oldRows.split("\n");
-    const newRows = [...splitRows];
-    const lastRow = splitRows.pop();
+    // Create log row
+    const getFullDate = getFullDateForTime(time);
+    const fullRow = `${getFullDate}; ${category.toUpperCase()}; ${desc || "-"}`;
+
+    await doBackup({ file });
+
+    const rows = await getRows({ file });
+    const newRows = [...rows];
+    const lastRow = rows.pop();
     const lastDate = parseRow(lastRow)?.date;
     const lastDateDate = lastDate ? parseISO(lastDate).getDate() : null;
     const isDateSame =
       lastDate !== null && lastDateDate === parseISO(getFullDate).getDate();
+
     if (!isDateSame) {
       const TIME_CHANGE_STR = "======";
       newRows.push(TIME_CHANGE_STR);
     }
+
     newRows.push(fullRow);
-    fs.writeFileSync(filePathFull, newRows.join("\n"));
+    //   fs.writeFileSync(filePathFull, newRows.join("\n"));
+    await writeRows({
+      rows: newRows,
+      file,
+    });
 
     console.log(`-> ${fullRow}`);
+  };
+
+const run = async () => {
+  const getConfig = await _getConfig(`${__dirname}/.config.json`);
+  const FILE = getConfig("file");
+  const filePathFull = getDirFullPath(FILE);
+
+  // Create file if doesn't exists
+  if (!fileExists(filePathFull)) {
+    fs.openSync(filePathFull, "w");
+    console.log(`File created: ${filePathFull}`);
   }
-}
+
+  // Command line args
+  const args = [...process.argv];
+  const params = args.slice(2);
+  const [cmd1, cmd2, cmd3, cmd4] = params;
+
+  const isCommandList = cmd1 === "list";
+  const isCommandRemove = cmd1 === "rm";
+  const isCommandOpen = cmd1 === "open";
+  //   const isCommandLog = !isCommandList;
+
+  if (isCommandList) {
+    await handleList({
+      file: filePathFull,
+    });
+  } else if (isCommandRemove) {
+    await handleRemove({
+      file: filePathFull,
+    });
+  } else if (isCommandOpen) {
+    await handleOpen({
+      file: filePathFull,
+    });
+  } else {
+    // Default is add new log
+    await handleAddLog({
+      getConfig,
+    })({
+      file: filePathFull,
+      params,
+    });
+  }
+};
+
+run()
+  .then(() => {})
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
