41bf8b20251fcdaf12cb04a403ac7c8ffb554f8d
samlinz <lindqvist.samuel@gmail.com>
2023-05-02 18:19:07 +0300

    Move files to src directory

--- test.js
+++
@@ -1,8 +0,0 @@
-// const { exec } = require("child_process");
-// const { log } = require("console");
-// const x = "'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'";
-// exec(x, (err, stdout, stderr) => {
-//   log(err);
-//   log(stdout);
-//   log(stderr);
-// });
